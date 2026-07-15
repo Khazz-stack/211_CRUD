ad6cd49021d76daf62ff8a4fa23efc1f7fa89222
Khazz <thelenaid@gmail.com>
2026-07-15 10:32:11 +0700

    listen

--- index.js
+++ index.js
@@ -93,7 +93,3 @@ app.delete('/biodata/:id', (req, res) => {
 app.listen(port, () => {
     console.log('App running on port ${port}.');
 });
-
-app.listen(port, () => {
-    console.log('App running on port ${port}.')
-})
